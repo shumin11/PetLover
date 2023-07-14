89df670db66bee9529f93e31ae9af35f6d72b78e
shumin11 <wsmin123456@hotmail.com>
2023-07-14 00:12:52 -0700

    add puppy images

--- FLASK/basic.py
+++ FLASK/basic.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/') #http://127.0.0.1:5000
 def index():
-    return "<h1>Hello Puppy!</h1>"
+    return render_template('basic.html')
+
+
+@app.route('/puppy_name/<name>') #http://127.0.0.1:5000/information
+def puppylatin(name):
+    
+    pupname = ''
+
+    if name[-1] == 'y':
+        pupname = name[:-1] + 'iful'
+    else:
+        pupname = name + 'y'
+
+    return "<h1> Your puppy latin name is: {}".format(pupname)
+
 
 if __name__ == '_main_':
-    app.run()
+    app.run(debug=True)
+
+
