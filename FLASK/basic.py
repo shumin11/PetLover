8932a666b492d06f4bd662f5751e5f956d495c1a
shumin11 <wsmin123456@hotmail.com>
2023-08-01 23:35:12 -0700

    template control flow

--- FLASK/basic.py
+++ FLASK/basic.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route('/') #http://127.0.0.1:5000
 def index():
-    return render_template('basic.html')
+    user_logged_in = False
 
+    name = "Shumin"
+    letters = list(name)
+    pup_dictionary = {'pup_name':'Sammy'}
+    puppies = ['Fluffy', 'Rufus', 'Spike']
+    
+    return render_template('basic.html', name = name, letters = letters, 
+                           pup_dictionary = pup_dictionary, puppies = puppies, user_logged_in = user_logged_in)
 
 @app.route('/puppy_name/<name>') #http://127.0.0.1:5000/information
 def puppylatin(name):
